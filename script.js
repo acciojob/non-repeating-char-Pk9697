9d5790a717af2f41b4119722b4dc52255fe0f573
Prashant Kumar <38308359+Pk9697@users.noreply.github.com>
2024-05-21 11:54:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	const mapp=new Map()
+	let ans=null
+	
+	str.split('').forEach(ch=>{
+		if(mapp.has(ch)){
+			mapp.set(ch,mapp.get(ch)+1)
+		}else{
+			mapp.set(ch,1) 
+		}
+	})
+	
+	str.split('').forEach(ch=>{
+		console.log(ch,mapp.get(ch))
+		if(mapp.get(ch)===1){		
+			ans=ch
+		}
+	})
+
+	return ans
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
